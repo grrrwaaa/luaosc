431a06018cfb10405d02ffa01e3e98858139b851
grrrwaaa <grrrwaaa@gmail.com>
2012-01-25 16:33:26 -0800

    updated lakefile

--- lakefile.lua
+++ lakefile.lua
@@ -1,17 +1,16 @@
 
 -- add the define OSC_HOST_BIG_ENDIAN for big-endian machines
 
-APR_1_INCLUDE_DIR = "/usr/include/apr-1/"	-- OSX
-APR_1_LIB_DIR = "/usr/lib/"
-APR_1_LIBS = 'apr-1'
+if PLAT == "Darwin" then
+	APR_1_INCLUDE_DIR = "/usr/include/apr-1/"	-- OSX
+	APR_1_LIB_DIR = "/usr/lib/"
+	APR_1_LIBS = 'apr-1'
+end
 
 cpp.shared{
 	"osc",
 	src = {
 		"luaopen_osc.cpp",
-		--"oscpack/osc/OscOutboundPacketStream.cpp",
-		--"oscpack/osc/OscReceivedElements.cpp",
-		--"oscpack/osc/OscTypes.cpp",
 	},
 	needs = {
 		"apr-1",
